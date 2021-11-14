9fca926b7be7f85047e1da664dd49df5cbd34a1d
MarcusSlover <mariuszek016@gmail.com>
2021-11-14 16:54:04 +0100

    - Test stuff

--- src/main/java/me/marcusslover/plus/PlusPlugin.java
+++ src/main/java/me/marcusslover/plus/PlusPlugin.java
@@ -1,9 +1,5 @@
 package me.marcusslover.plus;
 
-import me.marcusslover.plus.lib.Plus;
-import me.marcusslover.plus.lib.json.JsonWrapper;
-import me.marcusslover.plus.test.WorldConfig;
-import me.marcusslover.plus.test.WorldJson;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -14,13 +10,5 @@ public class PlusPlugin extends JavaPlugin {
     public void onEnable() {
         File dataFolder = getDataFolder();
         if (!dataFolder.exists()) dataFolder.mkdirs();
-
-        /*Test*/
-        WorldConfig worldConfig = new WorldConfig(Plus.file("world.yml"));
-        WorldJson worldJson = new WorldJson(Plus.file("world.json"));
-        JsonWrapper json = new JsonWrapper(worldJson.getJsonElement());
-
-
-
     }
 }
