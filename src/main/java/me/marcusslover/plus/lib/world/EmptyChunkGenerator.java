84bdd40e150d6f3e79f4b384b74f08d93a9d99f4
MarcusSlover <mariuszek016@gmail.com>
2021-11-20 14:37:36 +0100

    + Emtpy chunk generator

---
+++ src/main/java/me/marcusslover/plus/lib/world/EmptyChunkGenerator.java
@@ -0,0 +1,33 @@
+package me.marcusslover.plus.lib.world;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class EmptyChunkGenerator extends ChunkGenerator {
+
+
+    @SuppressWarnings("deprecation")
+    @NotNull
+    @Override
+    public ChunkGenerator.ChunkData generateChunkData(@NotNull World world, @NotNull Random random, int x, int z, @NotNull BiomeGrid biome) {
+        return createChunkData(world);
+    }
+
+    @Override
+    public boolean canSpawn(@NotNull World world, int x, int z) {
+        return true;
+    }
+
+    @Override
+    public Location getFixedSpawnLocation(@NotNull World world, @NotNull Random random) {
+        final Location spawnLocation = new Location(world, 0.0D, 64.0D, 0.0D);
+        final Location blockLocation = spawnLocation.clone().subtract(0D, 1D, 0D);
+        blockLocation.getBlock().setType(Material.BEDROCK);
+        return spawnLocation;
+    }
+}
