14401f5301e59fcb110e48ee9c75471c76c75ff8
EnderGamingFilms <endergamingfilms@gmail.com>
2022-10-13 21:58:27 -0700

    ✨ Better listener interface

---
+++ src/main/java/com/marcusslover/plus/lib/util/PlusListener.java
@@ -0,0 +1,15 @@
+package com.marcusslover.plus.lib.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+
+public interface PlusListener extends Listener {
+     default void register() {
+        Bukkit.getPluginManager().registerEvents(this, ServerUtils.getCallingPlugin());
+    }
+
+    default void unregister() {
+        HandlerList.unregisterAll(this);
+    }
+}
