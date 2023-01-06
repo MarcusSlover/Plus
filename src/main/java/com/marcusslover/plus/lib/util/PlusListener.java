95ced58544a9684765d42d39cdf80d49486ea8e7
Ender <endergamingfilms@gmail.com>
2023-01-06 15:20:16 -0800

    ⭕ Deprecate PlusListener
    - It was a bad idea to begin with.

--- src/main/java/com/marcusslover/plus/lib/util/PlusListener.java
+++ src/main/java/com/marcusslover/plus/lib/util/PlusListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 
+@Deprecated
 public interface PlusListener extends Listener {
      default void register() {
         Bukkit.getPluginManager().registerEvents(this, ServerUtils.getCallingPlugin());
