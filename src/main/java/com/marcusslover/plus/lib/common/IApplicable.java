c458bca20a9967a93cf400a8a1fcea03f57c278b
Ender <endergamingfilms@gmail.com>
2023-02-24 17:46:26 -0800

    ✨ Added predicate sendAll to Applicable interface

--- src/main/java/com/marcusslover/plus/lib/common/IApplicable.java
+++ src/main/java/com/marcusslover/plus/lib/common/IApplicable.java
@@ -3,8 +3,10 @@ package com.marcusslover.plus.lib.common;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.function.Predicate;
 
 public interface IApplicable<T extends LivingEntity, V> {
     @NotNull V apply(@NotNull T target);
@@ -25,19 +27,33 @@ public interface IApplicable<T extends LivingEntity, V> {
         return (V) this;
     }
 
-    default @NotNull V sendAll() {
-        return this.sendAll("");
-    }
-
-    default @NotNull V sendAll(String permission) {
-        permission = permission == null ? "" : permission;
-
+    default @NotNull V sendAll(@NotNull Predicate<T> predicate) {
         for (var sender : Bukkit.getOnlinePlayers()) {
-            if (permission.isBlank() || sender.hasPermission(permission)) {
+            if (sender == null) {
+                continue;
+            }
+
+            if (predicate.test((T) sender)) {
                 this.apply((T) sender);
             }
         }
 
         return (V) this;
+    }
+
+    default @NotNull V sendAll(@Nullable String permission) {
+        final String p = permission == null ? "" : permission;
+
+        return this.sendAll(sender -> {
+            if (p.isBlank()) {
+                return true;
+            }
+
+            return sender.hasPermission(p);
+        });
+    }
+
+    default @NotNull V sendAll() {
+        return this.sendAll("");
     }
 }
