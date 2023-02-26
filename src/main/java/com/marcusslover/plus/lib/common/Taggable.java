f59ab7ce1119e9931f5432509facd9e1cb7c7062
Ender <endergamingfilms@gmail.com>
2023-02-25 20:17:04 -0800

    🔨 Added MetaContainer & Taggble Interfaces
    - Cleans up the Item class a lot and allows for future helper classed that need to use Persistant data containers (such as and enities)

---
+++ src/main/java/com/marcusslover/plus/lib/common/Taggable.java
@@ -0,0 +1,51 @@
+package com.marcusslover.plus.lib.common;
+
+import com.marcusslover.plus.lib.item.Item;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataHolder;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public abstract class Taggable<V extends IMetaContainer<V, P>, P extends PersistentDataHolder> implements IMetaContainer<Item, ItemMeta> {
+    protected abstract @NotNull V holder();
+
+    public boolean hasTag(@NotNull String key, @NotNull PersistentDataType<?, ?> type) {
+        if (this.holder().meta() == null) {
+            return false;
+        }
+
+        PersistentDataContainer p = this.holder().meta().getPersistentDataContainer();
+        NamespacedKey n = new NamespacedKey("plus", key);
+
+        return p.has(n, type);
+    }
+
+    public @NotNull <T> V setTag(@NotNull String key, @NotNull PersistentDataType<T, T> type, @NotNull T value) {
+        this.holder().meta(itemMeta -> {
+            PersistentDataContainer p = itemMeta.getPersistentDataContainer();
+            NamespacedKey n = new NamespacedKey("plus", key);
+            p.set(n, type, value);
+        });
+
+        return this.holder();
+    }
+
+    public @NotNull <K> Optional<K> getTag(@NotNull String key, @NotNull PersistentDataType<K, K> type) {
+        if (this.holder().meta() == null) {
+            return Optional.empty();
+        }
+
+        PersistentDataContainer p = this.holder().meta().getPersistentDataContainer();
+        NamespacedKey n = new NamespacedKey("plus", key);
+
+        if (p.has(n, type)) {
+            return Optional.ofNullable(p.get(n, type));
+        }
+
+        return Optional.empty();
+    }
+}
