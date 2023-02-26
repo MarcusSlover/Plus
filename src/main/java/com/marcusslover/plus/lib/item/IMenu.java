ab1b5edce220dcd63cf9ed1f8b444bd0be4fe5f3
Mariusz Matyszczak <mariuszek016@gmail.com>
2023-02-26 08:56:57 +0100

    ✨ More stuff + menu populators and improved button mechanics.
    Took 2 hours 38 minutes

--- src/main/java/com/marcusslover/plus/lib/item/IMenu.java
+++ src/main/java/com/marcusslover/plus/lib/item/IMenu.java
@@ -1,17 +1,18 @@
 package com.marcusslover.plus.lib.item;
 
+import com.marcusslover.plus.lib.common.ISendable;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a menu.
  */
-public interface IMenu {
+public interface IMenu extends ISendable<IMenu> {
     /**
      * Opens the menu to the player.
      *
-     * @param canvas the canvas
+     * @param menuCanvas the canvas
      * @param player the player
      */
-    void open(@NotNull Canvas canvas, @NotNull Player player);
+    void open(@NotNull Canvas menuCanvas, @NotNull Player player);
 }
