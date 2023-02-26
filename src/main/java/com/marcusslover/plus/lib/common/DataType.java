a014a5c3b36c911aec90dd9cf0bf0015492fbc5d
Ender <endergamingfilms@gmail.com>
2023-02-25 20:40:50 -0800

    ✨ DataTypes need to be final

--- src/main/java/com/marcusslover/plus/lib/common/DataType.java
+++ src/main/java/com/marcusslover/plus/lib/common/DataType.java
@@ -3,14 +3,14 @@ package com.marcusslover.plus.lib.common;
 import org.bukkit.persistence.PersistentDataType;
 
 public record DataType() {
-    public static PersistentDataType<Byte, Byte> BYTE = PersistentDataType.BYTE;
-    public static PersistentDataType<Short, Short> SHORT = PersistentDataType.SHORT;
-    public static PersistentDataType<Integer, Integer> INTEGER = PersistentDataType.INTEGER;
-    public static PersistentDataType<Long, Long> LONG = PersistentDataType.LONG;
-    public static PersistentDataType<Float, Float> FLOAT = PersistentDataType.FLOAT;
-    public static PersistentDataType<Double, Double> DOUBLE = PersistentDataType.DOUBLE;
-    public static PersistentDataType<String, String> STRING = PersistentDataType.STRING;
-    public static PersistentDataType<byte[], byte[]> BYTE_ARRAY = PersistentDataType.BYTE_ARRAY;
-    public static PersistentDataType<int[], int[]> INTEGER_ARRAY = PersistentDataType.INTEGER_ARRAY;
-    public static PersistentDataType<long[], long[]> LONG_ARRAY = PersistentDataType.LONG_ARRAY;
+    public static final PersistentDataType<Byte, Byte> BYTE = PersistentDataType.BYTE;
+    public static final PersistentDataType<Short, Short> SHORT = PersistentDataType.SHORT;
+    public static final PersistentDataType<Integer, Integer> INTEGER = PersistentDataType.INTEGER;
+    public static final PersistentDataType<Long, Long> LONG = PersistentDataType.LONG;
+    public static final PersistentDataType<Float, Float> FLOAT = PersistentDataType.FLOAT;
+    public static final PersistentDataType<Double, Double> DOUBLE = PersistentDataType.DOUBLE;
+    public static final PersistentDataType<String, String> STRING = PersistentDataType.STRING;
+    public static final PersistentDataType<byte[], byte[]> BYTE_ARRAY = PersistentDataType.BYTE_ARRAY;
+    public static final PersistentDataType<int[], int[]> INTEGER_ARRAY = PersistentDataType.INTEGER_ARRAY;
+    public static final PersistentDataType<long[], long[]> LONG_ARRAY = PersistentDataType.LONG_ARRAY;
 }
