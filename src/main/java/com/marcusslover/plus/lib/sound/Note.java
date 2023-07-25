546247ec8683470736644fba289521d40084fab6
Mariusz Matyszczak <mariuszek016@gmail.com>
2023-07-25 21:31:02 +0200

    ✨More Note functions.

--- src/main/java/com/marcusslover/plus/lib/sound/Note.java
+++ src/main/java/com/marcusslover/plus/lib/sound/Note.java
@@ -22,12 +22,42 @@ public class Note implements ISendable<Note> {
         return new Note(Sound.sound(sound.key(), Sound.Source.MASTER, 1F, 1F));
     }
 
+    public static @NotNull Note of(@NotNull org.bukkit.Sound sound, long seed) {
+        var builder = Sound.sound()
+                .type(sound.key())
+                .source(Sound.Source.MASTER)
+                .volume(1F)
+                .pitch(1F)
+                .seed(seed);
+        return new Note(builder.build());
+    }
+
     public static @NotNull Note of(@NotNull org.bukkit.Sound sound, float volume, float pitch) {
         return new Note(Sound.sound(sound.key(), Sound.Source.MASTER, volume, pitch));
     }
 
+    public static @NotNull Note of(@NotNull org.bukkit.Sound sound, float volume, float pitch, long seed) {
+        var builder = Sound.sound()
+                .type(sound.key())
+                .source(Sound.Source.MASTER)
+                .volume(volume)
+                .pitch(pitch)
+                .seed(seed);
+        return new Note(builder.build());
+    }
+
     public static @NotNull Note of(@NotNull org.bukkit.Sound sound, float volume, float pitch, @NotNull Sound.Source source) {
         return new Note(Sound.sound(sound.key(), source, volume, pitch));
+    }
+
+    public static @NotNull Note of(@NotNull org.bukkit.Sound sound, float volume, float pitch, @NotNull Sound.Source source, long seed) {
+        var builder = Sound.sound()
+                .type(sound.key())
+                .source(source)
+                .volume(volume)
+                .pitch(pitch)
+                .seed(seed);
+        return new Note(builder.build());
     }
 
     public static @NotNull Note of(@NotNull Sound sound) {
@@ -38,28 +68,95 @@ public class Note implements ISendable<Note> {
         return new Note(Sound.sound(Key.key(sound), Sound.Source.MASTER, 1F, 1F));
     }
 
+    public static @NotNull Note of(@NotNull String sound, long seed) {
+        var builder = Sound.sound()
+                .type(Key.key(sound))
+                .source(Sound.Source.MASTER)
+                .volume(1F)
+                .pitch(1F)
+                .seed(seed);
+        return new Note(builder.build());
+    }
+
     public static @NotNull Note of(@NotNull String sound, float volume, float pitch) {
         return new Note(Sound.sound(Key.key(sound), Sound.Source.MASTER, volume, pitch));
+    }
+
+    public static @NotNull Note of(@NotNull String sound, float volume, float pitch, long seed) {
+        var builder = Sound.sound()
+                .type(Key.key(sound))
+                .source(Sound.Source.MASTER)
+                .volume(volume)
+                .pitch(pitch)
+                .seed(seed);
+        return new Note(builder.build());
     }
 
     public static @NotNull Note of(@NotNull String sound, float volume, float pitch, @NotNull Sound.Source source) {
         return new Note(Sound.sound(Key.key(sound), source, volume, pitch));
     }
 
+    public static @NotNull Note of(@NotNull String sound, float volume, float pitch, @NotNull Sound.Source source, long seed) {
+        var builder = Sound.sound()
+                .type(Key.key(sound))
+                .source(source)
+                .volume(volume)
+                .pitch(pitch)
+                .seed(seed);
+        return new Note(builder.build());
+    }
+
     public static @NotNull Note of(@NotNull Key key) {
         return new Note(Sound.sound(key, Sound.Source.MASTER, 1F, 1F));
+    }
+
+    public static @NotNull Note of(@NotNull Key key, long seed) {
+        var builder = Sound.sound()
+                .type(key)
+                .source(Sound.Source.MASTER)
+                .volume(1F)
+                .pitch(1F)
+                .seed(seed);
+        return new Note(builder.build());
     }
 
     public static @NotNull Note of(@NotNull Key key, float volume, float pitch) {
         return new Note(Sound.sound(key, Sound.Source.MASTER, volume, pitch));
     }
 
+    public static @NotNull Note of(@NotNull Key key, float volume, float pitch, long seed) {
+        var builder = Sound.sound()
+                .type(key)
+                .source(Sound.Source.MASTER)
+                .volume(volume)
+                .pitch(pitch)
+                .seed(seed);
+        return new Note(builder.build());
+    }
+
     public static @NotNull Note of(@NotNull Key key, float volume, float pitch, @NotNull Sound.Source source) {
         return new Note(Sound.sound(key, source, volume, pitch));
     }
 
+    public static @NotNull Note of(@NotNull Key key, float volume, float pitch, @NotNull Sound.Source source, long seed) {
+        var builder = Sound.sound()
+                .type(key)
+                .source(source)
+                .volume(volume)
+                .pitch(pitch)
+                .seed(seed);
+        return new Note(builder.build());
+    }
+
+    // more functions
     public @NotNull Note send(@NotNull Location location) {
         location.getWorld().playSound(this.sound, location.getX(), location.getY(), location.getZ());
+
+        return this;
+    }
+
+    public @NotNull Note send(@NotNull Location location, @NotNull Audience audience) {
+        audience.playSound(this.sound, location.getX(), location.getY(), location.getZ());
 
         return this;
     }
