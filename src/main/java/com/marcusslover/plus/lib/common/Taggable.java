5f6ae3bf8d2ec29caec7eae7f13a7dc69dec63b3
Mariusz Matyszczak <mariuszek016@gmail.com>
2023-10-27 12:24:24 +0200

    🚑️Tags.

--- src/main/java/com/marcusslover/plus/lib/common/Taggable.java
+++ src/main/java/com/marcusslover/plus/lib/common/Taggable.java
@@ -25,10 +25,7 @@ public abstract class Taggable<V extends IMetaContainer<V, P>, P extends Persist
      * @param key  The key of the tag.
      * @param type The type of the tag.
      * @return True if the item has the tag, false otherwise.
-     * @deprecated The method is deprecated because you can achieve the same result with {@link #tag(String, PersistentDataType)},
-     * where you can check if the returned {@link Optional} is empty or not.
      */
-    @Deprecated
     public boolean hasTag(@NotNull String key, @NotNull PersistentDataType<?, ?> type) {
         if (this.holder().meta() == null) {
             return false;
@@ -41,7 +38,7 @@ public abstract class Taggable<V extends IMetaContainer<V, P>, P extends Persist
     }
 
     // Alias for setTag()
-    public @NotNull <K> V tag(@NotNull String key, @NotNull K value, @NotNull PersistentDataType<K, K> type) {
+    public @NotNull <K, Z> V tag(@NotNull String key, @NotNull Z value, @NotNull PersistentDataType<K, Z> type) {
         return this.setTag(key, value, type);
     }
 
@@ -54,7 +51,7 @@ public abstract class Taggable<V extends IMetaContainer<V, P>, P extends Persist
      * @param <K>   The type of the value.
      * @return The holder.
      */
-    public @NotNull <K> V setTag(@NotNull String key, @NotNull K value, @NotNull PersistentDataType<K, K> type) {
+    public @NotNull <K, Z> V setTag(@NotNull String key, @NotNull Z value, @NotNull PersistentDataType<K, Z> type) {
         this.holder().meta(itemMeta -> {
             PersistentDataContainer p = itemMeta.getPersistentDataContainer();
             NamespacedKey n = new NamespacedKey("plus", key);
@@ -65,8 +62,24 @@ public abstract class Taggable<V extends IMetaContainer<V, P>, P extends Persist
     }
 
     // Alias for getTag()
-    public @NotNull <K> Optional<K> tag(@NotNull String key, @NotNull PersistentDataType<K, K> type) {
+    public @NotNull <K, Z> Optional<Z> tag(@NotNull String key, @NotNull PersistentDataType<K, Z> type) {
         return this.getTag(key, type);
+    }
+
+    /**
+     * Remove the tag with the given key.
+     *
+     * @param key The key of the tag.
+     * @return The holder.
+     */
+    public @NotNull V removeTag(@NotNull String key) {
+        this.holder().meta(itemMeta -> {
+            PersistentDataContainer p = itemMeta.getPersistentDataContainer();
+            NamespacedKey n = new NamespacedKey("plus", key);
+            p.remove(n);
+        });
+
+        return this.holder();
     }
 
     /**
@@ -77,7 +90,7 @@ public abstract class Taggable<V extends IMetaContainer<V, P>, P extends Persist
      * @param <K>  The type of the value.
      * @return The optional value of the tag. If the item doesn't have the tag, the optional will be empty.
      */
-    public @NotNull <K> Optional<K> getTag(@NotNull String key, @NotNull PersistentDataType<K, K> type) {
+    public @NotNull <K, Z> Optional<Z> getTag(@NotNull String key, @NotNull PersistentDataType<K, Z> type) {
         if (this.holder().meta() == null) {
             return Optional.empty();
         }
